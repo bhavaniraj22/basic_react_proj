4dc3f529967ba57fd4c22e441dc506b86d2bff35
bhavaniraj22 <bhavaniraj2002@gmail.com>
2025-01-06 01:06:03 +0530

    props

--- basics/src/index.js
+++ basics/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-<<<<<<< HEAD
-=======
 
->>>>>>> 3614089071ebd541a9288fd64d8d9199288ce075
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,7 +13,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-<<<<<<< HEAD
-
-=======
->>>>>>> 3614089071ebd541a9288fd64d8d9199288ce075
