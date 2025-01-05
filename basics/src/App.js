5fe4041614946ee485617313186fa5ee2427751e
bhavaniraj22 <bhavaniraj2002@gmail.com>
2025-01-06 00:51:30 +0530

    props concept

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,6 +1,19 @@
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact"
+/**
+ * Renders the main application component.
+ * 
+ * @returns {JSX.Element} The rendered application component.
+ */
 function App() {
   return (
+    <div>
     <h1>Hello</h1>
+    <Home name = {'Pooja'} age = {24} />
+    <About />
+    <Contact />
+    </div>
   );
 }
 
